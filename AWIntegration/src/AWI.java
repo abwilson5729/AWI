9839615fed1d8aaa204bb0d0f29b71367e22e624
abwilson5729 <abwilson5729@H339R1S2.STUDENT.ad.fgcu.edu>
2017-09-21 10:01:32 -0400

    Day 02 changes

--- AWIntegration/src/AWI.java
+++ AWIntegration/src/AWI.java
@@ -7,6 +7,36 @@ public class AWI {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to my Integration Project");
+		
+		
+		//Day 02
+		
+		/*byte: The byte data type is an 8-bit signed two's complement integer. 
+		short: The short data type is a 16-bit signed two's complement integer. 
+		int: use the int data type to represent an unsigned 32-bit integer
+		long: The long data type is a 64-bit two's complement integer. 
+		float: The float data type is a single-precision 32-bit floating point.
+		double: For decimal values, this data type is generally the default 
+			choice.
+		boolean: The boolean data type has  two possible values: true and false. 
+		char: The char data type is a single 16-bit Unicode character.
+		String: String objects are immutable, line of characters.
+		*/
+		
+		int i = 8;
+		String b = "bird";
+		double d = 8.3;
+		System.out.println();
+		System.out.println("Example of a int, String, and double: ");
+		System.out.println(i);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println("A variable is a location in memory");
+		System.out.println();
+		System.out.println("Additon of int i and int i, when i = 8");
+		System.out.println(i + i);
+		System.out.println("Division of int i by int i, when i = 8");
+		System.out.println(i / i);
 	}
 
 }
